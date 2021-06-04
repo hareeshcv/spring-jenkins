e55170710a11d63e79d177c685f98124ce1a035f
hareeshcv <chareesh8519@gmail.com>
2021-06-04 12:38:15 +0530

    added OVERRIDING

--- appcrud/src/main/java/com/example/demoapp/DemoappApplication.java
+++ appcrud/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoappApplication {
 
-//Test
+//venkat
 
     public static void main(String[] args) {
         SpringApplication.run(DemoappApplication.class, args);
