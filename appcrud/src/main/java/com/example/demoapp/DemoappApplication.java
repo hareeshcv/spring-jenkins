c7e61cb80b7d8faa82cdcf954a06f01603ec599c
hareeshcv <chareesh8519@gmail.com>
2021-06-04 12:34:55 +0530

    added comment line

--- appcrud/src/main/java/com/example/demoapp/DemoappApplication.java
+++ appcrud/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoappApplication {
 
+//Test
+
     public static void main(String[] args) {
         SpringApplication.run(DemoappApplication.class, args);
     }
